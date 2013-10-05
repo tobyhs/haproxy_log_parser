de70441620778253e8f641a1a2a9b1be1dca6c3c
Toby Hsieh <thrasher13b@gmail.com>
2013-10-05 16:01:41 -0700

    Change spec to use expect syntax

--- spec/haproxy_log_parser_spec.rb
+++ spec/haproxy_log_parser_spec.rb
@@ -7,68 +7,68 @@ describe HAProxyLogParser do
   describe '.parse' do
     it 'parses LINE[0] correctly' do
       entry = HAProxyLogParser.parse(LINES[0])
-      entry.client_ip.should == '10.0.8.2'
-      entry.client_port.should == 34028
-      entry.accept_date.should == Time.local(2011, 8, 9, 20, 30, 46, 429)
-      entry.frontend_name.should == 'proxy-out'
+      expect(entry.client_ip).to eq('10.0.8.2')
+      expect(entry.client_port).to eq(34028)
+      expect(entry.accept_date).to eq(Time.local(2011, 8, 9, 20, 30, 46, 429))
+      expect(entry.frontend_name).to eq('proxy-out')
       expect(entry).to be_ssl
-      entry.backend_name.should == 'proxy-out'
-      entry.server_name.should == 'cache1'
-      entry.tq.should == 1
-      entry.tw.should == 0
-      entry.tc.should == 2
-      entry.tr.should == 126
-      entry.tt.should == 128
-      entry.status_code.should == 301
-      entry.bytes_read.should == 223
-      entry.captured_request_cookie.should == {}
-      entry.captured_response_cookie.should == {}
-      entry.termination_state.should == '----'
-      entry.actconn.should == 617
-      entry.feconn.should == 523
-      entry.beconn.should == 336
-      entry.srv_conn.should == 168
-      entry.retries.should == 0
-      entry.srv_queue.should == 0
-      entry.backend_queue.should == 0
-      entry.captured_request_headers.should == ['www.sytadin.equipement.gouv.fr', '', 'http://trafic.1wt.eu/']
-      entry.captured_response_headers.should == ['Apache', '230', '', '', 'http://www.sytadin.']
-      entry.http_request.should == 'GET http://www.sytadin.equipement.gouv.fr/ HTTP/1.1'
+      expect(entry.backend_name).to eq('proxy-out')
+      expect(entry.server_name).to eq('cache1')
+      expect(entry.tq).to eq(1)
+      expect(entry.tw).to eq(0)
+      expect(entry.tc).to eq(2)
+      expect(entry.tr).to eq(126)
+      expect(entry.tt).to eq(128)
+      expect(entry.status_code).to eq(301)
+      expect(entry.bytes_read).to eq(223)
+      expect(entry.captured_request_cookie).to eq({})
+      expect(entry.captured_response_cookie).to eq({})
+      expect(entry.termination_state).to eq('----')
+      expect(entry.actconn).to eq(617)
+      expect(entry.feconn).to eq(523)
+      expect(entry.beconn).to eq(336)
+      expect(entry.srv_conn).to eq(168)
+      expect(entry.retries).to eq(0)
+      expect(entry.srv_queue).to eq(0)
+      expect(entry.backend_queue).to eq(0)
+      expect(entry.captured_request_headers).to eq(['www.sytadin.equipement.gouv.fr', '', 'http://trafic.1wt.eu/'])
+      expect(entry.captured_response_headers).to eq(['Apache', '230', '', '', 'http://www.sytadin.'])
+      expect(entry.http_request).to eq('GET http://www.sytadin.equipement.gouv.fr/ HTTP/1.1')
     end
 
     it 'parses LINES[1] correctly' do
       entry = HAProxyLogParser.parse(LINES[1])
-      entry.client_ip.should == '192.168.1.215'
-      entry.client_port.should == 50679
-      entry.accept_date.should == Time.local(2012, 5, 21, 1, 35, 46, 146)
-      entry.frontend_name.should == 'webapp'
+      expect(entry.client_ip).to eq('192.168.1.215')
+      expect(entry.client_port).to eq(50679)
+      expect(entry.accept_date).to eq(Time.local(2012, 5, 21, 1, 35, 46, 146))
+      expect(entry.frontend_name).to eq('webapp')
       expect(entry).to_not be_ssl
-      entry.backend_name.should == 'webapp_backend'
-      entry.server_name.should == 'web09'
-      entry.tq.should == 27
-      entry.tw.should == 0
-      entry.tc.should == 1
-      entry.tr.should == 0
-      entry.tt.should == 217
-      entry.status_code.should == 200
-      entry.bytes_read.should == 1367
-      entry.captured_request_cookie.should == {'session' => 'abc'}
-      entry.captured_response_cookie.should == {'session' => 'xyz'}
-      entry.termination_state.should == '----'
-      entry.actconn.should == 600
-      entry.feconn.should == 529
-      entry.beconn.should == 336
-      entry.srv_conn.should == 158
-      entry.retries.should == 0
-      entry.srv_queue.should == 0
-      entry.backend_queue.should == 0
-      entry.captured_request_headers.should == ['|| {5F41}', 'http://google.com/', '']
-      entry.captured_response_headers.should == ['1270925568', '', '']
-      entry.http_request.should == 'GET /images/image.gif HTTP/1.1'
+      expect(entry.backend_name).to eq('webapp_backend')
+      expect(entry.server_name).to eq('web09')
+      expect(entry.tq).to eq(27)
+      expect(entry.tw).to eq(0)
+      expect(entry.tc).to eq(1)
+      expect(entry.tr).to eq(0)
+      expect(entry.tt).to eq(217)
+      expect(entry.status_code).to eq(200)
+      expect(entry.bytes_read).to eq(1367)
+      expect(entry.captured_request_cookie).to eq({'session' => 'abc'})
+      expect(entry.captured_response_cookie).to eq({'session' => 'xyz'})
+      expect(entry.termination_state).to eq('----')
+      expect(entry.actconn).to eq(600)
+      expect(entry.feconn).to eq(529)
+      expect(entry.beconn).to eq(336)
+      expect(entry.srv_conn).to eq(158)
+      expect(entry.retries).to eq(0)
+      expect(entry.srv_queue).to eq(0)
+      expect(entry.backend_queue).to eq(0)
+      expect(entry.captured_request_headers).to eq(['|| {5F41}', 'http://google.com/', ''])
+      expect(entry.captured_response_headers).to eq(['1270925568', '', ''])
+      expect(entry.http_request).to eq('GET /images/image.gif HTTP/1.1')
     end
 
     it 'returns nil if the line is invalid' do
-      HAProxyLogParser.parse('asdf jkl;').should be_nil
+      expect(HAProxyLogParser.parse('asdf jkl;')).to be_nil
     end
   end
 end
